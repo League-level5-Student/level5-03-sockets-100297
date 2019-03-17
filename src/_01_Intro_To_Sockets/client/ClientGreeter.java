cc4217437afcc5ba9282eae2bca9dcdb4de9f23d
Your Name <you@example.com>
2019-03-17 12:56:10 -0700

    stop stalking me

--- src/_01_Intro_To_Sockets/client/ClientGreeter.java
+++ src/_01_Intro_To_Sockets/client/ClientGreeter.java
@@ -8,22 +8,29 @@ public class ClientGreeter {
    public static void main(String [] args) {
 	  //1. Create a String for the ip address of the server. 
 	  // If you don't know how to find a computer's ip address, ask about ifconfig on linux/mac and ipconfig on windows.
-      
+      String ipn = "192.168.1.176";
       //2. Create an integer for the server's port number
-      
+      int portn = 8080;
       //3. Surround steps 4-9 in a try-catch block that catches any IOExceptions.
-    
+
     	 //4. Create an object of the Socket class. When initializing the object, pass in the ip address and the port number
- 
+      	try {
+			Socket sock = new Socket(ipn, portn);
+			DataOutputStream a = (DataOutputStream) sock.getOutputStream();
          //5. Create a DataOutputStream object. When initializing it, use the Socket object you created in step 4 to call the getOutputStream() method.
          
          //6. Use the DataOutputStream object to send a message to the server using the writeUTF(String message) method
-         
+       a.writeUTF("This is a special message");  
          //7. Create a DataInputStream object. When initializing it, use the Server object you created in step 4 to call the getInputStream() method.
-         
+         DataInputStream inp = (DataInputStream) sock.getInputStream();
          //8. Use the DataInputStream object to print a message from the server using the readUTF() method.
-         
+          System.out.println(inp.readUTF());
          //9. Close the client's server object
-
+          sock.close();
+      	}
+     catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
    }
 }
--- src/_01_Intro_To_Sockets/server/ServerGreeter.java
+++ src/_01_Intro_To_Sockets/server/ServerGreeter.java
@@ -1,27 +1,39 @@
 package _01_Intro_To_Sockets.server;
 
 import java.net.*;
+
+import javax.swing.JOptionPane;
+
 import java.io.*;
 
 public class ServerGreeter extends Thread {
 	//1. Create an object of the ServerSocket class
-
+		ServerSocket ssock;
 	public ServerGreeter() throws IOException {
 		//2. Initialize the ServerSocket object. In the parameters,
 		//   you must define the port at which the server will listen for connections.
-		
+		ssock = new ServerSocket(8080);
 		//*OPTIONAL* you can set a time limit for the server to wait by using the 
 		//  ServerSocket's setSoTimeout(int timeInMilliSeconds) method
 	}
 
 	public void run() {
 		//3. Create a boolean variable and initialize it to true.
-		
+		boolean bool = true;
 		//4. Make a while loop that continues looping as long as the boolean created in the previous step is true.
-			
+			while(bool == true) {
+				
+					try {
+						System.out.println("The server is waiting for some to connect to\"");
+						Socket soc = ssock.accept();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+			}
 			//5. Make a try-catch block that checks for two types Exceptions: SocketTimeoutException and IOException.
 			//   Put steps 8 - 15 in the try block.
-		
+			
 				//8. Let the user know that the server is waiting for a client to connect.
 		
 				//9. Create an object of the Socket class and initialize it to serverSocket.accept();
